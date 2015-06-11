8021c1261bfb7fd6fa9973f78722c1e044e1a3a4
ands <ands>
2015-06-11 16:20:33 +0000

    made display a class. made client a class using a state machine.

--- client/client.py
+++ client/client.py
@@ -1,54 +1,111 @@
-#import logging
 import time
 import pygame
-
-pygame.mixer.pre_init(44100, -16, 1, 512)
 import display
 import sounds
 import hardware
-import network
-myid = 32
-i = 0
-fireButton = 0
-
-sounds = sounds.Sounds()
-hardware = hardware.Hardware()
 from hardware import i2cAddresses
-#cs = network.Client_server("10.0.8.200", 9005)
+import network
+from enum import Enum
 
-pygame.init()
+class ClientState(Enum):
+	IDLE = 0
+	SHOOTING = 1
+	COOLDOWN = 2
+	DEAD = 3
 
-hardware.setHitpointLED(i2cAddresses.HITPOINT_WEAPON, 0, 255, 0)
-hardware.setWeaponCharacteristics(playerid=32, damage=30, laser_duration=1)#randint(1,254)
-
-try:
-	while True:
-		#print network.Client_server.empf()
-		fireButton = hardware.isWeaponButtonDown(0) 
-		enable, playerid, dmg = hardware.getWeaponHitResults()
-		if enable and playerid != myid:
-			print playerid
-			hardware.setHitpointLED(i2cAddresses.HITPOINT_WEAPON, 0, 0, 0)
-			#cs.send("death")
-			sounds.play('tod', True)
-			sounds.play('down', True)
-			hardware.setHitpointLED(i2cAddresses.HITPOINT_WEAPON, 0, 255, 0)
-			hardware.getWeaponHitResults() # flush in case of another hit
-			fireButton = 0
+class Client:
+	def __init__(self, pygame_instance):
+		self.pygame = pygame_instance
 		
-		if fireButton == 1 and display.schuss >=0:
-			sounds.play('pew')
-			hardware.setWeaponLasers(laser0=1, laser1=0)
-			hardware.setWeaponLED(2, 0, 0)
-			time.sleep(0.1)
-			hardware.setWeaponLasers(laser0=0, laser1=0)
-			hardware.setWeaponLED(0, 0, 0)
-			i= i+1
-			display.schuss = display.schuss-1
-		print fireButton, i, display.schuss
-except KeyboardInterrupt:
-	print 'Keyboard interrupt...'
-	hardware.setWeaponLasers(laser0=0, laser1=0)
-	hardware.setHitpointLED(i2cAddresses.HITPOINT_WEAPON, 0, 0, 0)
-	hardware.setWeaponLED(0, 0, 0)
+		#config
+		self.myid = 32
+		self.health = 100.0
+		self.ammo = 1000
+		self.shotDuration = 100 #milliseconds
+		self.cooldownDuration = 400 #milliseconds
+		self.deathDuration = 5000 #milliseconds
+		
+		#runtime state
+		self.shotEnd = 0
+		self.cooldownEnd = 0
+		self.deathEnd = 0
+		self.state = ClientState.IDLE
+		self.sounds = sounds.Sounds()
+		self.display = display.Display(self.pygame, self.health, self.ammo)
+		self.hardware = hardware.Hardware()
+		#self.cs = network.Client_server("10.0.8.200", 9005)
+
+		#initialize hardware
+		self.hardware.setHitpointLED(i2cAddresses.HITPOINT_WEAPON, 0, 255, 0)
+		self.hardware.setWeaponCharacteristics(playerid=32, damage=30, laser_duration=self.shotDuration/100)
+
+	def shoot(self):
+		if self.ammo > 0:
+			self.state = ClientState.SHOOTING
+			self.shotEnd = self.milliseconds + self.shotDuration
+			self.ammo = self.ammo - 1
+			self.display.setAmmo(self.ammo)
+			self.sounds.play('pew')
+			self.hardware.shootWeapon(laser0=1, laser1=0);
+	
+	def handleDeath(self):
+		enable, playerid, dmg = self.hardware.getWeaponHitResults()
+		if enable and playerid != myid:
+			self.state = ClientState.DEAD
+			self.deathEnd = self.milliseconds + self.deathDuration
+			print "killed by player ", playerid
+			#self.cs.send("death")
+			self.sounds.play('tod')
+			self.hardware.setHitpointLED(i2cAddresses.HITPOINT_WEAPON, 0, 0, 0)
+
+	def update(self):
+		self.milliseconds = self.pygame.time.get_ticks()
+		
+		#print network.Client_server.empf() where should this go?
+		
+		#state machine
+		if self.state == ClientState.IDLE:
+			if self.hardware.isWeaponButtonDown(0):
+				self.shoot()
+			self.handleDeath()
+			
+		elif self.state == ClientState.SHOOTING:
+			#go back to idle state if the shot is over
+			if self.milliseconds >= self.shotEnd:
+				self.state = ClientState.COOLDOWN
+				self.cooldownEnd = self.milliseconds + self.cooldownDuration
+				#TODO: reset hardware here later!
+			self.handleDeath()
+			
+		elif self.state == ClientState.COOLDOWN:
+			if self.milliseconds >= self.cooldownEnd:
+				self.state = ClientState.IDLE
+			self.handleDeath()
+			
+		elif self.state == ClientState.DEAD:
+			#reset hardware and jump to idle state
+			if self.milliseconds >= self.deathEnd:
+				self.state = ClientState.IDLE
+				self.hardware.setHitpointLED(i2cAddresses.HITPOINT_WEAPON, 0, 255, 0)
+				self.hardware.getWeaponHitResults() # flush in case of another hit
+
+		self.display.redraw()
+
+	def shutdown(self):
+		print 'Shutting down...'
+		self.hardware.setWeaponLasers(laser0=0, laser1=0)
+		self.hardware.setHitpointLED(i2cAddresses.HITPOINT_WEAPON, 0, 0, 0)
+		self.hardware.setWeaponLED(0, 0, 0, 0)
+
+if __name__ == '__main__':
+	pygame.mixer.pre_init(44100, -16, 1, 512)
+	pygame.init()
+	
+	client = Client(pygame)
+	try:
+		while True:
+			client.update()
+	except KeyboardInterrupt:
+		client.shutdown()
+	
 	pygame.quit()
--- client/display.py
+++ client/display.py
@@ -1,37 +1,44 @@
 import time
 import pygame
-import random
-import thread
 
-global schuss
-schuss = 1000
+class Display:
+	def __init__(self, pygame_instance, health, ammo):
+		self.pygame = pygame_instance
+		self.pygame.mouse.set_visible(False)
+		self.screen = self.pygame.display.set_mode([240, 320])
+		self.font1 = self.pygame.font.SysFont("comicsansms", 72)
+		self.font2 = self.pygame.font.SysFont("comicsansms", 25)
+		self.sprite_weapon = self.pygame.image.load("images/waffe.jpg")
+		self.sprite_heart = self.pygame.image.load("images/herz.png")
+		
+		self.last_clock = ""
+		self.setHealth(health)
+		self.setAmmo(ammo)
+		
+	def setAmmo(self, ammo):
+		self.text_ammo = self.font1.render(str(ammo), True, (0, 128, 0))
+		
+	def setHealth(self, health):
+		self.text_health = self.font1.render(str(health), True, (0,128,0))
 
-pygame.init()
-pygame.mouse.set_visible(False)
-screen = pygame.display.set_mode([240, 320])
-font1 = pygame.font.SysFont("comicsansms", 72)
-font2 = pygame.font.SysFont("comicsansms", 25)
-bild_waffe = pygame.image.load("images/waffe.jpg")
-bild_herz = pygame.image.load("images/herz.png")
-
-def neu():
-	while 1:
-		time.sleep(0.1)
-		text_schuss = font1.render(str(schuss), True, (0, 128, 0))
-		text_uhrzeit = font2.render(str(time.strftime("%H:%M:%S")), True, (0, 255,0))
-		text_leben = font1.render("100%", True, (0,128,0))
-		screen.fill((255, 255, 255))
-		pygame.draw.rect(screen,[0,0,0],[0,0,240,17],0)
-		pygame.draw.rect(screen,[0,255,0],[5,5,3,10],0)
-		pygame.draw.rect(screen,[0,255,0],[10,8,3,7],0)
-		pygame.draw.rect(screen,[0,255,0],[15,12,3,3],0)
-		pygame.draw.rect(screen,[0,255,0],[0,70,240,3],0)
-		pygame.draw.rect(screen,[0,255,0],[0,120,240,3],0)
-		screen.blit(bild_waffe, (0, 30))
-		screen.blit(bild_herz, (0, 75))
-		screen.blit(text_schuss,(100 - text_schuss.get_width() // 2, 45 - text_schuss.get_height() // 2))
-		screen.blit(text_leben,(100 - text_leben.get_width() // 2, 95 - text_leben.get_height() // 2))
-		screen.blit(text_uhrzeit,(175, 0))
-		pygame.display.update()
-
-thread.start_new_thread(neu,())
+	def redraw(self):
+		# update clock text
+		current_clock = str(time.strftime("%H:%M:%S"))
+		if current_clock != self.last_clock:
+			self.text_clock = self.font2.render(current_clock, True, (0, 255,0))
+			self.last_clock = current_clock
+	
+		# clear and redraw everything
+		self.screen.fill((255, 255, 255))
+		self.pygame.draw.rect(self.screen,[0,0,0],[0,0,240,17],0)
+		self.pygame.draw.rect(self.screen,[0,255,0],[5,5,3,10],0)
+		self.pygame.draw.rect(self.screen,[0,255,0],[10,8,3,7],0)
+		self.pygame.draw.rect(self.screen,[0,255,0],[15,12,3,3],0)
+		self.pygame.draw.rect(self.screen,[0,255,0],[0,70,240,3],0)
+		self.pygame.draw.rect(self.screen,[0,255,0],[0,120,240,3],0)
+		self.screen.blit(self.sprite_weapon, (0, 30))
+		self.screen.blit(self.sprite_heart, (0, 75))
+		self.screen.blit(self.text_ammo, (100 - self.text_ammo.get_width() // 2, 45 - self.text_ammo.get_height() // 2))
+		self.screen.blit(self.text_health, (100 - self.text_health.get_width() // 2, 95 - self.text_health.get_height() // 2))
+		self.screen.blit(self.text_clock, (175, 0))
+		self.pygame.display.flip()
